81a2e14fcee0c7e5ff3aead80ea4ae93ce013292
Aryan Singh Kushwah <kushwahyuvraj82@gmail.com>
2026-08-17 11:36:46 +0530

    Add greet and add functions with user input

---
+++ Basics/functions.py
@@ -0,0 +1,13 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+
+def add(a, b):
+    return a + b
+
+
+name = input("Enter your name: ")
+print(greet(name))
+
+result = add(10, 20)
+print("Sum:", result)
