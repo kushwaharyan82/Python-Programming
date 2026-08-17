97438662191e7156178be47150e21407bf6a0d0b
Aryan Singh Kushwah <kushwahyuvraj82@gmail.com>
2026-08-17 11:41:11 +0530

    Add basic set operations in sets.py

---
+++ Lists-Tuples-Sets/sets.py
@@ -0,0 +1,11 @@
+numbers = {10, 20, 30, 20, 40, 10}
+
+print("Set:", numbers)
+
+numbers.add(50)
+print("After adding:", numbers)
+
+numbers.remove(20)
+print("After removing:", numbers)
+
+print("Length:", len(numbers))
