19310c4e35410f17f4509c126b8983d6676e4517
Aryan Singh Kushwah <kushwahyuvraj82@gmail.com>
2026-08-17 11:37:16 +0530

    Add string operations for user input

---
+++ Basics/string_operations.py
@@ -0,0 +1,7 @@
+text = input("Enter a string: ")
+
+print("Original:", text)
+print("Length:", len(text))
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("Reversed:", text[::-1])
