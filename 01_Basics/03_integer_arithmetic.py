033991cda6d804824a1bffa4957d047db1921d3f
Aryan Singh Kushwah <kushwahyuvraj82@gmail.com>
2026-08-19 10:41:20 +0530

    Add integer arithmetic progarms

---
+++ 01_Basics/03_integer_arithmetic.py
@@ -0,0 +1,72 @@
+# 21. Sum of two integers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum:", a + b)
+
+
+# 22. Difference of two integers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Difference:", a - b)
+
+
+# 23. Product of two integers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Product:", a * b)
+
+
+# 24. Quotient of two integers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Quotient:", a // b)
+
+
+# 25. Remainder of two integers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Remainder:", a % b)
+
+
+# 26. Add three integers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("Sum:", a + b + c)
+
+
+# 27. Subtract one number from another
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Result:", a - b)
+
+
+# 28. Multiply three integers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("Product:", a * b * c)
+
+
+# 29. Divide an integer by 2
+number = int(input("Enter a number: "))
+
+print("Result:", number / 2)
+
+
+# 30. Average of three integers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+average = (a + b + c) / 3
+
+print("Average:", average)
