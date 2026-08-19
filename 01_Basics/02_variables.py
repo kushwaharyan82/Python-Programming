12169fe7140a04be63f84ab714ea8ef66e2862f3
Aryan Singh Kushwah <kushwahyuvraj82@gmail.com>
2026-08-19 10:39:30 +0530

    Added variables and input basics

---
+++ 01_Basics/02_variables.py
@@ -0,0 +1,56 @@
+# 11. Declare an integer variable and assign a value
+number = 10
+print(number)
+
+
+# 12. Declare a float variable and assign a value
+number = 10.5
+print(number)
+
+
+# 13. Declare a character variable and assign a value
+character = "A"
+print(character)
+
+
+# 14. Declare multiple variables in one line
+a, b, c = 10, 20, 30
+print(a, b, c)
+
+
+# 15. Print values of variables after assigning
+name = "Aryan"
+age = 19
+
+print(name)
+print(age)
+
+
+# 16. Use a constant for PI
+PI = 3.14
+print(PI)
+
+
+# 17. Change the value of a variable
+number = 10
+
+print("Before:", number)
+
+number = 20
+
+print("After:", number)
+
+
+# 18. Input and display an integer
+number = int(input("Enter an integer: "))
+print("You entered:", number)
+
+
+# 19. Input and display a float
+number = float(input("Enter a float: "))
+print("You entered:", number)
+
+
+# 20. Input and display a character
+character = input("Enter a character: ")
+print("You entered:", character)
