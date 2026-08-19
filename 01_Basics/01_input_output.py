380842e02e91d6e028b3d89a1a3fdab0e158d47e
Aryan Singh Kushwah <kushwahyuvraj82@gmail.com>
2026-08-19 10:37:34 +0530

    Added input output basics
    Add examples of basic input and output operations in Python.

---
+++ 01_Basics/01_input_output.py
@@ -0,0 +1,62 @@
+# 1. Print Hello World
+print("Hello, World!")
+
+
+# 2. Print name, age and country
+name = "Aryan"
+age = 19
+country = "India"
+
+print(name)
+print(age)
+print(country)
+
+
+# 3. Print a number
+print(100)
+
+
+# 4. Print a float number
+print(10.5)
+
+
+# 5. Print a character
+print("A")
+
+
+# 6. Print integer, float and character
+integer = 10
+floating = 10.5
+character = "A"
+
+print(integer, floating, character)
+
+
+# 7. Use \n and \t
+print("Name:\tAryan\nAge:\t19")
+
+
+# 8. Print decimal, octal and hexadecimal
+number = 25
+
+print("Decimal:", number)
+print("Octal:", oct(number))
+print("Hexadecimal:", hex(number))
+
+
+# 9. Display multiple lines
+print("Hello")
+print("Welcome to Python")
+print("Learning Programming")
+
+
+# 10. Size of basic Python objects
+import sys
+
+integer = 10
+floating = 10.5
+character = "A"
+
+print("Integer size:", sys.getsizeof(integer))
+print("Float size:", sys.getsizeof(floating))
+print("Character size:", sys.getsizeof(character))
